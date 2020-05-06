ae83d7a18541af843b41dea92ab8c4c08e4370d0
Brayan Forero <=brayanroswll2802@gmail.com>
2020-05-06 09:29:28 -0400

    Crea la clase Utilities en el paquete Config y modifica la clase Base para mejor manejo de constantes

--- src/Config/Base.java
+++ src/Config/Base.java
@@ -8,20 +8,12 @@ import java.util.logging.Logger;
 
 public class Base {
 
-    private final String SERVER = "127.0.0.1";
-    private final String PORT = "3306";
-    private final String BDNAME = "colegio_santa_rita";
-    private final String BDUSER = "admin";
-    private final String USERPASS = "admin123*";
-    private final String DRIVER = "com.mysql.jdbc.Driver";
-    private final String URL = "jdbc:mysql://" + this.SERVER + ":" + this.PORT + "/" + this.BDNAME;
-
     public Connection getConnection() throws SQLException {
         Connection con = null;
 
         try {
-            Class.forName(DRIVER);
-            con = (Connection) DriverManager.getConnection(this.URL, this.BDUSER, this.USERPASS);
+            Class.forName(Utilities.DRIVER);
+            con = (Connection) DriverManager.getConnection(Utilities.URL, Utilities.BDUSER, Utilities.USERPASS);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Base.class.getName()).log(Level.SEVERE, null, ex);
             System.err.println("ERROR EN LA CONEXION: " + ex);
---
+++ src/Config/Utilities.java
@@ -0,0 +1,12 @@
+package Config;
+
+public class Utilities {
+
+    public static final String SERVER = "127.0.0.1";
+    public static final String PORT = "3306";
+    public static final String BDNAME = "colegio_santa_rita";
+    public static final String BDUSER = "admin";
+    public static final String USERPASS = "admin123*";
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://" +SERVER + ":" + PORT + "/" + BDNAME;
+}
