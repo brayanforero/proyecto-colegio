781519e907818aeba14280132d261a14b4aa3362
Brayan Forero <=brayanroswll2802@gmail.com>
2020-05-08 08:22:08 -0400

    Agrega el las clase App, punto de partida de la aplicacion

---
+++ src/Controllers/App.java
@@ -0,0 +1,20 @@
+
+package Controllers;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class App extends Application{
+
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+    
+}
