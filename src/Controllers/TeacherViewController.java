cf8e2b046b46faca84366599e42d7451431d276a
Brayan Forero <=brayanroswll2802@gmail.com>
2020-05-20 17:41:04 -0400

    Trae el turno del grado, la fecha del sistema y el periodo

--- src/Controllers/TeacherViewController.java
+++ src/Controllers/TeacherViewController.java
@@ -11,17 +11,15 @@ import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 
-/**
- * FXML Controller class
- *
- * @author Brayan
- */
 public class TeacherViewController implements Initializable {
 
     @FXML
@@ -34,10 +32,15 @@ public class TeacherViewController implements Initializable {
     private Label lblNameUser;
     @FXML
     private Label lblidUser;
+    @FXML
+    private Label lblPeriodo;
+    @FXML
+    private Label lblTurno;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        Date date = new Date();SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        this.lblDate.setText(format.format(date) + "");
     }
 
     public void initAtributeUser(int id, String nameUser) {
@@ -52,6 +55,7 @@ public class TeacherViewController implements Initializable {
             alert.setHeaderText(null);
             alert.setContentText("Estimado Usuario los Datos son Incorrectos");
             alert.showAndWait();
+            Platform.exit();
 
         }
 
@@ -65,14 +69,32 @@ public class TeacherViewController implements Initializable {
             PreparedStatement ps = con.prepareCall("call sp_grado_asignado(?)");
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            
+
             if (rs.next()) {
-                this.lblDegress.setText( rs.getString("nivel") );
-                this.lblSeccion.setText( rs.getString("seccion") );
+                this.lblDegress.setText(rs.getString("nivel"));
+                this.lblSeccion.setText(rs.getString("seccion"));
+                this.lblTurno.setText(rs.getString("turno"));
+                this.lblPeriodo.setText(rs.getString("periodo"));
+
+                ps.close();
+            } else {
+                ps.close();
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle(null);
+                alert.setHeaderText(null);
+                alert.setContentText("Error: Solitud de datos no puedo ser procesada");
+                alert.showAndWait();
+                Platform.exit();
             }
-            ps.close();
+
         } catch (SQLException e) {
             System.err.println("Error en traer los datos:" + e);
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(null);
+            alert.setHeaderText(null);
+            alert.setContentText("Error: Solitud de datos no puedo ser procesada");
+            alert.showAndWait();
+            Platform.exit();
         }
     }
 
