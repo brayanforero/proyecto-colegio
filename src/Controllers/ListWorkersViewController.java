6174ef6d9c3883c554d293c4ca7d4bc0054838f8
Brayan Forero <brayanroswll2802@gmail.com>
2020-10-29 05:50:36 -0400

    Genera pdf del personal

--- src/Controllers/ListWorkersViewController.java
+++ src/Controllers/ListWorkersViewController.java
@@ -1,13 +1,20 @@
 package Controllers;
 
+import Config.Base;
+import Files.Pdf;
 import Models.Workers;
+import com.mysql.jdbc.Connection;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -31,18 +38,16 @@ public class ListWorkersViewController implements Initializable {
     private TableColumn colCargoWorker;
     @FXML
     private TableColumn colPhoneWorker;
+    @FXML
+    private Button btnExportsWorkers;
 
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         this.setTableWorker();
     }
 
+    public void setTableWorker() {
 
-
-    public void setTableWorker(){
-        
-        
         this.colIdWorker.setCellValueFactory(new PropertyValueFactory("id"));
         this.colDocWorker.setCellValueFactory(new PropertyValueFactory("identification"));
         this.colNameWorker.setCellValueFactory(new PropertyValueFactory("names"));
@@ -54,6 +59,17 @@ public class ListWorkersViewController implements Initializable {
         this.tableWorkers.setItems(this.itemsWorkers);
     }
 
-    
-    
+    @FXML
+    private void generatePdf(ActionEvent event) {
+
+        try {
+            Connection con = Base.getConnectionStatic();
+            Pdf pdf = new Pdf("personal", null, con);
+            pdf.generate();
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ListWorkersViewController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
