0ab69e0d4f265269f8c2cafdea0434b4ccca96ab
Brayan Forero <brayanroswll2802@gmail.com>
2020-11-06 16:35:08 -0400

    Redimenciona el login, agrega combobox al registro de personal

--- src/Controllers/NewWorkerViewController.java
+++ src/Controllers/NewWorkerViewController.java
@@ -8,18 +8,16 @@ package Controllers;
 import Models.Workers;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class NewWorkerViewController implements Initializable {
 
     @FXML
@@ -34,23 +32,40 @@ public class NewWorkerViewController implements Initializable {
     private TextField txtPhoneTelefono;
     @FXML
     private Button btnSaveWorker;
-    @FXML
     private TextField txtCargoWorker;
-
+    @FXML
+    private ComboBox<String> cboTypeDoc;
+    ObservableList<String> items = FXCollections.observableArrayList();
+    @FXML
+    private ComboBox<String> cboPosition;
+    ObservableList<String> itemsPositions = FXCollections.observableArrayList();
    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+       initCombo();
     }    
-
+    
+    public void initCombo (){
+        
+        this.items.addAll("V-", "E-");
+        this.cboTypeDoc.setItems(this.items);
+        
+        this.cboTypeDoc.getSelectionModel().select("V-");
+        
+        this.itemsPositions.addAll("DIRECTOR", "COORDINADOR", "DOCENTE", "OBRERO", "SECRETARIA");
+        this.cboPosition.setItems(this.itemsPositions);
+        
+        this.cboPosition.getSelectionModel().select(1);
+    }
     @FXML
     private void savedWorker(ActionEvent event) {
         
-        String doc = this.txtDocWorker.getText().toUpperCase();
+       
+        String doc = this.cboTypeDoc.getValue() + this.txtDocWorker.getText().toUpperCase();
         String name = this.txtNameWorker.getText().toUpperCase();
         String lastName = this.txtLastNameWorker.getText().toUpperCase();
         String email = this.txtEmailWorker.getText().toUpperCase();
-        String cargo = this.txtCargoWorker.getText().toUpperCase();
+        String cargo = this.cboPosition.getValue();
         String phone = this.txtPhoneTelefono.getText();
         
         Workers worker = new Workers(name, lastName, doc);
