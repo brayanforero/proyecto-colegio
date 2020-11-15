0db87440108a02cc10b13f71cc5b257f7877465c
Brayan Forero <brayanroswll2802@gmail.com>
2020-11-15 04:13:08 -0400

    Agrega tabla de usuario a vista y elimina item de la vista admin

--- src/Controllers/NewUserViewController.java
+++ src/Controllers/NewUserViewController.java
@@ -1,4 +1,3 @@
-
 package Controllers;
 
 import Models.Users;
@@ -14,10 +13,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class NewUserViewController implements Initializable {
-    
+
     @FXML
     private TextField txtNameUser;
     @FXML
@@ -32,62 +34,91 @@ public class NewUserViewController implements Initializable {
     private ObservableList<Workers> itemsWorkers = FXCollections.observableArrayList();
     @FXML
     private Button btnReset;
+    @FXML
+    private TableView<Users> tableUser;
+    private ObservableList<Users> itemsUser = FXCollections.observableArrayList();
+    @FXML
+    private TableColumn colIdUser;
+    @FXML
+    private TableColumn colNameUser;
+    @FXML
+    private TableColumn colPerson;
 
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-        this.option = FXCollections.observableArrayList();
+
+        this.initCombo();
+        this.setTableUser();
+    }
+
+    public void setTableUser() {
+        this.itemsUser.clear();
+        this.colIdUser.setCellValueFactory(new PropertyValueFactory("id"));
+        this.colNameUser.setCellValueFactory(new PropertyValueFactory("name"));
+        this.colPerson.setCellValueFactory(new PropertyValueFactory("workers"));
+        Users.getUserHabilited(this.itemsUser);
+        this.tableUser.setItems(this.itemsUser);
+    }
+    
+    public void initCombo(){
+       this.option = FXCollections.observableArrayList();
         this.option.addAll("ADMINSTRADOR", "DOCENTE");
         this.cboRolUser.setItems(this.option);
         this.setWorkersCombo();
-        
+
         this.cboRolUser.getSelectionModel().select(0);
-        this.cboWorkers.getSelectionModel().select(0);
-    }    
-    
+        this.cboWorkers.getSelectionModel().select(0); 
+    }
+
     @FXML
     private void newUser(ActionEvent event) {
-        
-        
+
         Workers worker = this.cboWorkers.getValue();
         String userName = this.txtNameUser.getText().toUpperCase();
         String userPass = this.txtPassUser.getText().toUpperCase();
         String userRol = this.cboRolUser.getValue();
-        
-        if(userName.isEmpty() || userPass.isEmpty() ){
-            
+
+        if (userName.isEmpty() || userPass.isEmpty()) {
+
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setHeaderText(null);
             alert.setContentText("Debe rellenar los campos vacios");
             alert.setTitle("ERROR");
             alert.showAndWait();
-            
+
             return;
         }
-        
+
         Users user = new Users(worker.getId(), userName, userPass, userRol);
-        
+
         Alert msg = user.newUser();
-        
+
         msg.showAndWait();
-        
-        
+        this.reset();
+        this.setTableUser();
     }
-    
-    public void setWorkersCombo(){
-        
+
+    public void setWorkersCombo() {
+
         Workers.setWorkerCombo(this.itemsWorkers);
         this.cboWorkers.setItems(this.itemsWorkers);
     }
 
     @FXML
     private void reset(ActionEvent event) {
-        
+
         this.txtNameUser.setText("");
         this.txtPassUser.setText("");
         this.cboRolUser.getSelectionModel().select(0);
         this.cboWorkers.getSelectionModel().select(0);
     }
     
+    private void reset() {
+
+        this.txtNameUser.setText("");
+        this.txtPassUser.setText("");
+        this.cboRolUser.getSelectionModel().select(0);
+        this.cboWorkers.getSelectionModel().select(0);
+    }
+
 }
