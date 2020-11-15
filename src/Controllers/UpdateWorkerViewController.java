9c08ae1ff9a7ed9bd3c48f4f5b1043cf017adfbe
Brayan Forero <brayanroswll2802@gmail.com>
2020-11-15 01:44:17 -0400

    Agrega validaciones a vista de actualizacion de personal

--- src/Controllers/UpdateWorkerViewController.java
+++ src/Controllers/UpdateWorkerViewController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Config.Utilities;
 import Models.Workers;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -55,12 +56,15 @@ public class UpdateWorkerViewController implements Initializable {
     private TableColumn colName;
     @FXML
     private TableColumn colLastName;
+    @FXML
+    private Button btnReset;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         this.setCombox();
         this.paneData.setVisible(false);
         this.setTable();
+        this.initEvet();
     }
 
     public void setCombox() {
@@ -91,6 +95,37 @@ public class UpdateWorkerViewController implements Initializable {
 
     @FXML
     private void saveData(ActionEvent event) {
+        
+        if (this.txtDocWorker.getText().length() < 8) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("Formato no valido para una cédula");
+            a.setHeaderText(null);
+            a.showAndWait();
+            return;
+        }
+        if (this.txtPhoneWorker.getText().length() < 7) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("Formato no valido para un número telefonico");
+            a.setHeaderText(null);
+            a.showAndWait();
+            return;
+        }
+        
+        if (!Utilities.validEmail(this.txtEmailWorker.getText())) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("Formato de email no valido");
+            a.setHeaderText(null);
+            a.showAndWait();
+            return;
+        }
+        if (this.txtNameWorker.getText().isEmpty() || this.txtNameWorker.getText().isEmpty()) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setContentText("Completa los campos");
+            a.setHeaderText(null);
+            a.showAndWait();
+            return;
+        }
+
         int id = Integer.parseInt(this.txtIdWorker.getText());
         String doc = this.cboDocWorker.getValue() + this.txtDocWorker.getText().toUpperCase();
         String name = this.txtNameWorker.getText().toUpperCase();
@@ -101,6 +136,7 @@ public class UpdateWorkerViewController implements Initializable {
         Workers worker = new Workers(id, cargo, email, phone, name, lastName, doc);
         Alert msg = worker.UpdateWorker();
         msg.showAndWait();
+        this.txtSearchWorker.setText("");
         this.paneData.setVisible(false);
         this.setTable();
     }
@@ -123,9 +159,25 @@ public class UpdateWorkerViewController implements Initializable {
     }
 
 
-    @FXML
     private void searchWorkerFilter(KeyEvent event) {
         setTable();
+    }
+    
+    public void initEvet(){
+        this.txtSearchWorker.setOnKeyTyped(e->Utilities.onlyLetter(e));
+        this.txtDocWorker.setOnKeyTyped(e->Utilities.onlyDigit(e));
+        this.txtPhoneWorker.setOnKeyTyped(e->Utilities.onlyDigit(e));
+        this.txtNameWorker.setOnKeyTyped(e->Utilities.onlyLetter(e));
+        this.txtLastNameWorker.setOnKeyTyped(e->Utilities.onlyLetter(e));
+        
+        this.txtSearchWorker.setOnKeyTyped(e->{
+            this.setTable();
+        });
+    }
+
+    @FXML
+    private void reset(ActionEvent event) {
+        this.paneData.setVisible(false);
     }
 
 }
